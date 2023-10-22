96d4fbd8988fa3a4f62a0306c07773ac2aab8700
devrewoh <137814665+devrewoh@users.noreply.github.com>
2023-10-22 10:39:34 -0600

    Added a letter counting function

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ def main():
     text = get_book_text(book_path)
     word_count = count_words(text)
     print(f"Word count: {word_count}")
+    letter_count = count_letters(text)
+    print(f"Letter count: {letter_count}")
+
+    #letter_count = count_letters(text)
 
 def get_book_text(path):
     with open(path) as f:
@@ -11,5 +15,17 @@ def get_book_text(path):
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_letters(text):
+    letter_count = {}
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+                "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    for letter in alphabet:
+        letter_count[letter] = 0
+        for char in text:
+            char = char.lower()
+            if char in letter_count:
+                letter_count[char] += 1
+    return letter_count
     
 main()
